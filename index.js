b1908c378ef2b7c7524b0a49e6c89417308c1627
shivamxaryan <shivamaryan64@gmail.com>
2023-03-23 23:46:43 +0530

    assets

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port =8000;
 const app=express();
 const expressLayouts= require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
 
 //setting us view engine

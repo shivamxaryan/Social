ebe449fb0471f9d3c38025792d80772153c1aefd
shivamxaryan <shivamaryan64@gmail.com>
2023-03-25 02:13:32 +0530

    sign up and log in page creation

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,22 @@
+const { model } = require("mongoose");
+
 module.exports.profile=function(req,res){
     return res.render('user_profile',{
         title:'Users Profile'
+    });
+}
+
+
+//rendering Log in page
+module.exports.logIn=function(req,res){
+    return res.render('user_log_in',{
+        title:'Codial | Log In'
+    });
+}
+
+//rendering Sing Up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:'Codial | Sign Up'
     });
 }
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,7 @@ const userPost=require('../controller/post_controller');
 
 router.get('/profile',userProfile.profile);
 router.get('/post',userPost.posts);
+router.get('/log-in',userProfile.logIn);
+router.get('/sign-up',userProfile.signUp);
 
 module.exports=router;

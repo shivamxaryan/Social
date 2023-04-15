d0ed0ca1372ae171b748168e08054ced4219bfee
shivamxaryan <shivamaryan64@gmail.com>
2023-04-15 18:50:26 +0530

    successfully deleting comments

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -33,29 +33,46 @@ module.exports.create=function(req,res){
 }
 
 
-module.exports.destroy=function(req,res){
-    Comment.findById(req.params.id)
-    .then(function(comment){
-        if(comment.user == req.user.id){
+module.exports.destroy=async function(req,res){
+    // Comment.findById(req.params.id)
+    // .then(function(comment){
+    //     if(comment.user == req.user.id){
             
-            let postId=comment.post;
-            comment.remove();
+    //         let postId=comment.post;
+    //         comment.remove();
 
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}})
-            .then(function(post){
-                return res.redirect('back');
-            })
-            .catch(function(err){
-                return res.redirect('back');
-            })
+    //         Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}})
+    //         .then(function(post){
+    //             return res.redirect('back');
+    //         })
+    //         .catch(function(err){
+    //             return res.redirect('back');
+    //         })
+    //     }
+    //     else{
+    //         console.log('user is not authorised');
+    //         return;
+    //     }
+    // })
+    // .catch(function(err){
+    //     console.log('error in deleting the comment');
+    //     return res.redirect('back');
+    // })
+
+    try{
+        const comment=await Comment.findById(req.params.id);
+        if(comment.user == req.user.id) {
+            // await comment.deleteOne();
+            let postId=comment.post;
+            await comment.deleteOne();
+
+            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}});
+           
         }
-        else{
-            console.log('user is not authorised');
-            return;
-        }
-    })
-    .catch(function(err){
-        console.log('error in deleting the comment');
         return res.redirect('back');
-    })
+
+    }catch(error){
+        return res.send({error:error,msg:'Somenthing went wrong in comment deleting!!'});
+    }
+    
 }

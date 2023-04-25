657624500485fa90a3fbd0f0469b4f9d73882535
shivamxaryan <shivamaryan64@gmail.com>
2023-04-26 00:09:01 +0530

    using ajax in creating post (only uploading)

---
+++ assets/js/home_post.js
@@ -0,0 +1,23 @@
+{
+    let createPost=function(){
+        let newPostForm=$('#new-post-form');
+
+    newPostForm.submit(function(e){                     //here 'e' is event
+        e.preventDefault();
+
+        
+        $.ajax({
+            type:'post',
+            url:'/posts/create',
+            data:newPostForm.serialize(),
+            success: function(data){
+                console.log(data);
+            },error:function(error){
+                console.log(error.responseText);
+            }
+        })
+    })
+    }
+
+    createPost();
+}

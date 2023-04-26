975496a52e0d1399384e180c00ff28caa3cfd2d0
shivamxaryan <shivamaryan64@gmail.com>
2023-04-26 23:39:40 +0530

    sorting the post

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -1,42 +1,42 @@
 {
     //method to submit form data of new post using ajax
-    let createPost=function(){
-        let newPostForm=$('#new-post-form');
+    let createPost = function () {
+        let newPostForm = $('#new-post-form');
 
-    newPostForm.submit(function(e){                     //here 'e' is event
-        e.preventDefault();
+        newPostForm.submit(function (e) {                     //here 'e' is event
+            e.preventDefault();
 
-        
-        $.ajax({
-            type:'post',
-            url:'/posts/create',
-            data:newPostForm.serialize(),
-            success: function(data){
-                let newPost=newPostDom(data.data.post);
-                $('#post-list-container>ul').prepend(newPost);
-            },error:function(error){
-                console.log(error.responseText);
-            }
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function (data) {
+                    let newPost = newPostDom(data.data.post);
+                    $('#post-list-container>ul').prepend(newPost);
+                }, error: function (error) {
+                    console.log(error.responseText);
+                }
+            })
         })
-    })
     }
 
 
     //method to create post in dom
-    let newPostDom=function(post){
+    let newPostDom = function (post) {
         return $(`<li id="post-${post._id}">
         <p>
         
            
                 <small>
-                        <a class="delete-post-button" href="/posts/destroy/${ post.id }">X</a>
+                        <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
                 </small>
               
     
-                ${post.content }
+                ${post.content}
                 <br>
                 <small>
-                ${ post.user.name }
+                ${post.user.name}
                 </small>
         </p>
     
@@ -44,19 +44,21 @@
                 
                 <form action="/comments/create" method="post">
                         <input type="text" name="content" placeholder="Type here to add comment..." required>
-                        <input type="hidden" name="post"  value="${ post._id }">
+                        <input type="hidden" name="post"  value="${post._id}">
                         <input type="submit" value="Add Comment">
                 </form>
                
         </div>
     
         <div class="post-comments-list">
-                <ul id="post-comments-${post._id }">
+                <ul id="post-comments-${post._id}">
                 
                 </ul>
         </div>
     </li>`)
     }
+
+
 
     createPost();
 }
